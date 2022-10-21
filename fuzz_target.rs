yuntongzhang/fuzz-rustc

commit 4ed773d97fa2001756a070a5ded34c222cb80799

Add mutator (uses spans from rustc_parse)

fuzz_target.rs
@@ -1,5 +1,21 @@
 #![no_main]
 #[macro_use] extern crate libfuzzer_sys;
+mod mutator;
+mod tst_mutator;
+use std::sync::Arc;
+use std::io::{self, Write};
+use std::sync::Mutex;
+
+// A threadsafe buffer. Stolen from rust-lang/rls/blob/78a36bd334b7e51726ea506919e0a4189c416855/rls/src/build/mod.rs
+struct TSWriter(Arc<Mutex<Vec<u8>>>);
+impl Write for TSWriter {
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        self.0.lock().unwrap().write(buf)
+    }
+    fn flush(&mut self) -> io::Result<()> {
+        self.0.lock().unwrap().flush()
+    }
+}
 
 /// rustc_driver::Callbacks object that stops before codegen.
 pub struct FuzzCallbacks;
@@ -80,24 +96,71 @@ impl rustc_codegen_ssa::traits::CodegenBackend for NullCodegenBackend {
 }
 
 
+fn rustc_args(input: &str) -> Vec<String> {
+    let mut v = vec![
+        "rustc".to_string(),
+        INPUT_PATH.to_string(),
+          "-o".to_string(),
+          "dummy_output_file".to_string(),
+        ];
+
+    // Pass through certain valid compile flags in the input. (In regression tests, these use e.g. "compile-fags:")
+
+    if input.contains("--edition=2015") || input.contains("edition:2015") {
+        v.push("--edition=2015".to_string());
+    }
+    else if input.contains("--edition=2018") || input.contains("edition:2018") {
+        v.push("--edition=2018".to_string());
+    }
+    else {
+        // Always specify some edition
+        v.push("--edition=2021".to_string());
+    }
+
+    if input.contains("--diagnostic-width=20") {
+        v.push("--diagnostic-width=20".to_string());
+    }
+
+    if input.contains("--error-format json") || input.contains("--error-format=json") {
+        v.push("--error-format=json".to_string());
+    }
+    else if input.contains("--error-format short") || input.contains("--error-format=short") {
+        v.push("--error-format=short".to_string());
+    }
+
+    //v.push("-Zverbose".to_string());
+    v.push("-L".to_string());
+    v.push("FUZZ_RUSTC_LIBRARY_DIR".to_string());
+    v
+}
+
 pub fn main_fuzz(input: String) {
+    //let input_byte_len = input.len();
+    let args = rustc_args(&input);
     let file_loader = Box::new(FuzzFileLoader::new(input));
     let mut callbacks = FuzzCallbacks;
+    let rse = Arc::default();
     let _result = rustc_driver::catch_fatal_errors(|| {
-        let args = &["rustc".to_string(),
-              INPUT_PATH.to_string(),
-              "-o".to_string(),
-              "dummy_output_file".to_string(),
-              "--edition".to_string(),
-              "2018".to_string(),
-              "-L".to_string(),
-              env!("FUZZ_RUSTC_LIBRARY_DIR").to_string()];
-        let mut run_compiler = rustc_driver::RunCompiler::new(args, &mut callbacks);
+        let rse_inner = Arc::clone(&rse);
+        let mut run_compiler = rustc_driver::RunCompiler::new(&args, &mut callbacks);
         run_compiler.set_file_loader(Some(file_loader));
         run_compiler.set_make_codegen_backend(
             Some(Box::new(|_| {Box::new(NullCodegenBackend)})));
+        run_compiler.set_emitter(Some(Box::new(TSWriter(rse_inner))));
         run_compiler.run()
     }).and_then(|result| result);
+
+    let _stderr = Arc::try_unwrap(rse).expect("Compilation is done").into_inner().unwrap();
+    //let stderr_byte_len = stderr.len();
+    //match String::from_utf8(stderr) {
+    //    Ok(s) => {
+    //        if stderr_byte_len > 1000 && stderr_byte_len > input_byte_len * 30 && !s.contains("unknown start of token") && !s.contains("character constant must be escaped") && !s.contains("delimiter") {
+    //            println!("{}", s);
+    //            panic!("Very long output");
+    //        }
+    //    }
+    //    Err(_) => { panic!("Non UTF-8 diagnostics from run_compiler"); }
+    //}
 }
 
 fuzz_target!(|data: &[u8]| {
@@ -105,9 +168,35 @@ fuzz_target!(|data: &[u8]| {
         return;
     }
     if let Ok(t) = String::from_utf8(data.into()) {
-        if let Some(_) = t.find("derive") {
-            return;
-        }
         main_fuzz(t);
+    }
+});
+
+fuzz_mutator!(|data: &mut [u8], size: usize, max_size: usize, seed: u32| {
+    use rand::{rngs::StdRng, Rng, SeedableRng};
+    if false { tst_mutator::exercise_mutator(); }
+    match String::from_utf8(data[0..size].to_vec()) {
+        Ok(original_source) => {
+            let mut rng = StdRng::seed_from_u64(seed as u64);
+            if rng.gen_bool(0.9) && original_source.is_ascii() {
+                //println!("Custom mutator");
+                let mu = mutator::ProgramMutator::new(original_source);
+                let new_source = mu.random_mutation(&mut rng).unwrap();
+                let new_data = new_source.as_bytes();
+                let new_size = std::cmp::min(max_size, new_data.len());
+                data[..new_size].copy_from_slice(&new_data[..new_size]);
+                new_data.len()
+            } else {
+                //println!("Basic mutator");
+                //Todo: for non-ascii files, sometimes try the 'reduction' of normalizing the file to ASCII
+                libfuzzer_sys::fuzzer_mutate(data, size, max_size)
+            }
+        }
+        _ => {
+            // No changes: mutator can't handle it and rustc probably rejects it early anyway
+            //eprintln!("How did this non-UTF-8 get in here?");
+            //println!("No changes");
+            size
+        }
     }
 });

Cargo.toml
@@ -6,6 +6,15 @@ edition = "2021"
 
 [dependencies]
 libfuzzer-sys = "0.4.0"
+rand = "0.8.5"
+strum = { version = "0.24", features = ["derive"] }
+strum_macros = "0.24"
+
+[dependencies.rustc_parse]
+path = "./rust/compiler/rustc_parse"
+
+[dependencies.rustc_ast]
+path = "./rust/compiler/rustc_ast"
 
 [dependencies.rustc_data_structures]
 path = "./rust/compiler/rustc_data_structures"

tst_mutator.rs
@@ -1,0 +1,148 @@
+// This isn't a test module because:
+// * That seems to mess up the compiler caching
+// * This is a mix of exploration and regression-testing
+// * It's hard to regression-test random coe
+// * That seems to run the code in a different context that makes panics behave differently, and
+//   a large part of what we're trying to do here is ensure that no panics escape from 
+//   deep in the compiler and stop the fuzzer during mutation.
+
+use crate::mutator;
+use rand::{rngs::StdRng, SeedableRng};
+
+fn tt(source: &'static str, expected_outputs: &'static [&'static str]) {
+    println!("");
+    println!("{}", source);
+    println!("");
+    const SHOW_ITERS: usize = 10;
+    const ITERS: usize = 10000;
+    let mut r = StdRng::seed_from_u64(0);
+    let s = mutator::ProgramMutator::new(source.to_string());
+
+    // Show spans, if we have them
+    println!("  {} spans:", s.ts.len());
+    for i in &s.ts {
+        println!("    {}: {}..{} [{}]", i.tag, i.lo, i.hi, source.get(i.lo .. i.hi).unwrap());
+    }
+
+    // Print a few (SHOW_ITERS) results.
+    // Compute many (ITER) results and count how many of them match each of the expected_outputs.
+    let mut cts = vec![0; expected_outputs.len()];
+    let mut error_ct = 0;
+    println!("  First {} mutations:", SHOW_ITERS);
+    for i in 0..ITERS {
+        let m = s.random_mutation(&mut r);
+        if let Ok(m) = m {
+            if i < SHOW_ITERS {
+                println!("    #{}: {}", i, m);
+            }
+            for i in 0 .. expected_outputs.len() {
+                if expected_outputs[i] == m {
+                    cts[i] += 1;
+                    break;
+                }
+            }
+        } else { 
+            error_ct += 1;
+        }
+    }
+    for i in 0 .. expected_outputs.len() {
+        println!("Created {}/{} of: {}", cts[i], ITERS, expected_outputs[i]);
+    }
+    if error_ct > 0 {
+        println!("... and {} errors", error_ct);
+    }
+}
+
+
+pub fn exercise_mutator() -> ! {
+    tt("fn main() { println!(\"{}\", 1); }", &[]);
+    tt("fn main() { println!(\"{}\", 2 + 3); }", &[]);
+    tt("fn main() { 5; }", &[]);
+    tt("fn main() { 8 + + 9; }", &[]);
+    tt("fn main() { 7 ", &[]);
+    tt("fn f() -> u32 { } ", &[]);
+    
+    // Test insertion on an empty program
+    tt(
+        "",
+        &[
+            "/",
+            "X",
+            "()",
+        ]
+    );
+
+    // Test byte-oriented insertion, including delimiter pairs
+    tt(
+        "ABC",
+        &[
+            "AXC",
+            "AC",
+            "AB[]C",
+            "A{B}C",
+            "{ABC}",
+        ]
+    );
+
+    // Test insertion. Certain spots should be more likely to be chosen as insertion points.
+    tt(
+        "fn main() {}",
+        &[
+            "Xfn main() {}",
+            "fXn main() {}",
+            "fnX main() {}",
+            "fn Xmain() {}",
+            "fn main() {X}",
+            "fn main() {}X",
+        ]
+    );
+
+    // Test swapping and stuff
+    tt(
+        "fn peh(b: bool) -> u32 { match b { false => 0, _ => 1 } }",
+        &[
+            // Copy the first pattern to the second
+            "fn peh(b: bool) -> u32 { match b { false => 0, false => 1 } }",
+            // Copy the second pattern to the first
+            "fn peh(b: bool) -> u32 { match b { _ => 0, _ => 1 } }",
+            // Swap the arms
+            "fn peh(b: bool) -> u32 { match b { _ => 1, false => 0 } }",
+            // Swap the results
+            "fn peh(b: bool) -> u32 { match b { false => 1, _ => 0 } }",
+            // Swap the patterns
+            "fn peh(b: bool) -> u32 { match b { _ => 0, false => 1 } }",
+        ]
+    );
+
+    tt(
+        "fn main() { std::str::from_utf8(&[65]).unwrap(); }",
+        &[]
+    );
+
+    // Swap generic params
+    tt(
+        "fn _k<'a, T>(_t: T) { }",
+        &["fn _k<T, 'a>(_t: T) { }"]
+    );
+
+    tt(
+        "fn main() { let _u = 4; }",
+        &[]
+    );
+
+    // This one makes the some part of rustc sad, so the parser doesn't give me an AST
+    tt("fn main() { 6; } #syntaxerrors#", &[]);
+
+    // This one makes the lexer fail, so we can't even create a parser
+    tt("}", &[]);
+
+    // This one is why we need catch_unwind
+    tt("use { f() }", &[]);
+
+    // This one is why we need `parse_sess.span_diagnostic.reset_err_count()` after parsing
+    // (catch_unwind fails to catch it for some reason!)
+    tt("impl<U> Foo<U", &[]);
+
+    println!("Done!");
+    std::process::abort();
+}

commit 2208f1cf658e17fd64a65378c17bc3ec7c668d63

Restore proper use of env var FUZZ_RUSTC_LIBRARY_DIR
This allows the fuzzed programs to use std again. Fixes a bug I introduced in 4ed773d97fa2001756a070a5ded34c222cb80799.

fuzz_target.rs
@@ -131,7 +131,7 @@ fn rustc_args(input: &str) -> Vec<String> {
 
     //v.push("-Zverbose".to_string());
     v.push("-L".to_string());
-    v.push("FUZZ_RUSTC_LIBRARY_DIR".to_string());
+    v.push(env!("FUZZ_RUSTC_LIBRARY_DIR").to_string());
     v
 }
 