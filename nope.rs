7cfeb66ef9db6218f215fc3eb638f63116f99917
Jesse Ruderman <jruderman@gmail.com>
2022-10-24 20:51:33 -0700

    Update comment

--- nope.rs
+++ nope.rs
@@ -14,7 +14,7 @@ pub fn do_not_even_parse(prog: &str) -> bool {
 
 // Making the parser recurse too deep will make it run out of stack space,
 // which can show up as several types of exits (abort, sigsegv on a guard page, or worse)
-// It can also lead to very large diagnostics, due to all the ASCII art arrows trying to point past each other
+// It can also lead to very large diagnostics if a single line is very long (https://github.com/rust-lang/rust/issues/103429)
 // Also it's often just slow, without being so slow (e.g. exponential) that it's worth reporting a bug
 // (RUST_MIN_STACK=20000000 wasn't enough to stop the crashes?)
 fn might_nest_too_deep(prog: &str) -> bool {
