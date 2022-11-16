1ed60ad07ed5ea7e183f5cb44fcffbfb51e4deba
Jesse Ruderman <jruderman@gmail.com>
2022-11-15 22:11:27 -0800

    Update nope.rs and timecpx.rs

--- nope.rs
+++ nope.rs
@@ -4,6 +4,10 @@ pub fn do_not_even_parse(prog: &str) -> bool {
         //eprintln!("Nope: might nest too deep");
         return true;
     }
+    if highest_nesting_angle_brackets(prog) > 30 {
+        // https://github.com/rust-lang/rust/issues/103620: temporarily here rather than in timecpx(?)
+        return true;
+    }
     false
 }
 
@@ -22,26 +26,66 @@ fn might_nest_too_deep(prog: &str) -> bool {
     ct > 400
 }
 
+fn highest_nesting_angle_brackets(input: &str) -> usize {
+    let mut highest_nest_level: usize = 0;
+    let mut current_nest_level: usize = 0;
+    for b in input.as_bytes() {
+        match b {
+            b'<' => {
+                current_nest_level += 1;
+                if highest_nest_level < current_nest_level {
+                    highest_nest_level = current_nest_level;
+                }
+            }
+            b'>' => {
+                current_nest_level = current_nest_level.saturating_sub(1);
+            }
+            _ => {}
+        }
+    }
+    highest_nest_level
+}
 
 pub fn do_not_compile(prog: &str) -> bool {
     if do_not_even_parse(prog) {
         return true;
     }
-    if prog.contains("rustc_strict_coherence") {
-        // Semi-intentional ICE: https://github.com/rust-lang/rust/issues/103753
-        return true;
-    }
-    if prog.contains("BikeshedIntrinsicFrom") {
-        // Let's give this nightly-only feature a rest for a while:
+    if prog.contains("BikeshedIntrinsicFrom") || prog.contains("rustc_layout") {
         // https://github.com/rust-lang/rust/issues/103634
-        // https://github.com/rust-lang/rust/issues/103751
-        // https://github.com/rust-lang/rust/issues/103783
         return true;
     }
     if prog.contains("generic_const_exprs") || prog.contains("adt_const_params") {
-        // Let's give this nightly-only feature a rest for a while:
         // https://github.com/rust-lang/rust/issues/103770
-        // https://github.com/rust-lang/rust/issues/103790 (even without the feature gate, but this still helps)
+        // https://github.com/rust-lang/rust/issues/104040
+        // https://github.com/rust-lang/rust/issues/94846
+        // https://github.com/rust-lang/rust/issues/101036
+        return true;
+    }
+    if prog.contains("specialization") {
+        // https://github.com/rust-lang/rust/issues/103708
+        return true;
+    }
+    if prog.contains("inherent_associated_types") {
+        // https://github.com/rust-lang/rust/issues/104240
+        // https://github.com/rust-lang/rust/issues/104260
+        return true;
+    }
+    if prog.contains("return_position_impl_trait_in_trait") {
+        // https://github.com/rust-lang/rust/issues/104281
+        return true;
+    }
+    if prog.contains("async") && prog.contains("impl") && prog.contains("for") {
+        // https://github.com/rust-lang/rust/issues/104281  ?
+        return true;
+    }
+    //if prog.contains("trait") && prog.contains("<") && prog.contains("const") && prog.contains("impl") {
+    //    // https://github.com/rust-lang/rust/issues/104312
+    //    return true;
+    //}
+    if prog.contains("cfg") && prog.contains("derive") {
+        return true;
+    }
+    if prog.contains("cfg_attr") {
         return true;
     }
 
--- timecpx.rs
+++ timecpx.rs
@@ -10,20 +10,34 @@ pub fn expected_dur(input: &str) -> Option<Duration> {
     let size = input.len() as u128;
     let ibytes = input.as_bytes();
 
-    let ct_equals = ibytes.iter().filter(|b| **b == b'=').count() as u128;
-
     let ct_ampers = ibytes.iter().filter(|b| **b == b'&').count() as u128;
+    let ct_bang = ibytes.iter().filter(|b| **b == b'!').count() as u128;
+    let ct_ques = ibytes.iter().filter(|b| **b == b'?').count() as u128;
 
-    let highest_nesting_level = highest_nesting(input);
+    let highest_nesting_level = highest_nesting_normal_delims(input);
 
     // let longest_line_len ........... (https://github.com/rust-lang/rust/issues/103429 diagnostic arrow messes) (when not error-format=short)
 
     if
         // Only fully exclude things that are _frequently_, _annoyingly_ slow *and* we're confident that the
-        // we have a way of exclusing (some) hits in a way that doesn't significantly decrease fuzzer coverage
-        // See below for issue numbers
-        ct_ampers > 300
-        //ct_equals > 400 ||   // this is pointless because even 100 hits the thing below
+        // we have a way of excluding (some) hits in a way that doesn't significantly decrease fuzzer coverage
+
+        // https://github.com/rust-lang/rust/issues/103421
+        ct_ampers > 300 ||
+
+        // idk what this is about; i couldn't even reproduce the slowness outside of the fuzzer
+        ct_ques > 100 ||
+
+        // Const eval can be arbitrarily slow if the limit is set high enough
+        // (hmm, it might be better to mangle this instead of excluding it)
+        // Revisit after #103877 lands
+        input.contains("const_eval_limit") ||
+
+        // Macros can and do blow up. Examples include:
+        //     src/test/ui/issues/issue-65131.rs (10^3)
+        //     src/test/ui/enum/issue-42747.rs (4^27) (#104162)
+        // It would be nice to have a more precise way to limit this, perhaps with help from compiler internals
+        (input.contains("macro_rules") && ct_bang > 3)
     {
         eprintln!("timecpx is recommending to skip this input");
         None
@@ -43,18 +57,17 @@ pub fn expected_dur(input: &str) -> Option<Duration> {
 
             // Various exponential or quadratic-and-slow things that just aren't that interesting
             // These numbers are arbitrary
-            200 * 2_u128.pow(highest_nesting_level.min(40) as u32) +
-
-            // https://github.com/rust-lang/rust/issues/103411
-            // "1=();" meaused at 75ms per repetition
-            300_000 * ct_equals
+            200 * 2_u128.pow(highest_nesting_level.min(40) as u32)
         };
 
         // Mul some factors like computer speed and whether debug assertions are enabled?
         // Slower getting started? Is it the sancov?
 
         if allowance_us > 30_000_000 {
-            eprintln!("timecpx is concerned this testcase could take {} seconds", (allowance_us as f64) / 1_000_000.0);
+            eprintln!(
+                "timecpx is concerned this testcase could take {} seconds",
+                (allowance_us as f64) / 1_000_000.0
+            );
             None
         } else {
             Some(Duration::from_micros(allowance_us as u64))
@@ -72,9 +85,9 @@ pub fn check_dur(elapsed: Duration, allowance: Duration) {
     }
 }
 
-fn highest_nesting(input: &str) -> usize {
-    let mut highest_nest_level : usize = 0;
-    let mut current_nest_level : usize = 0;
+fn highest_nesting_normal_delims(input: &str) -> usize {
+    let mut highest_nest_level: usize = 0;
+    let mut current_nest_level: usize = 0;
     for b in input.as_bytes() {
         match b {
             b'(' | b'[' | b'{' => {
@@ -86,7 +99,7 @@ fn highest_nesting(input: &str) -> usize {
             b')' | b']' | b'}' => {
                 current_nest_level = current_nest_level.saturating_sub(1);
             }
-            _ => { }
+            _ => {}
         }
     }
     highest_nest_level
