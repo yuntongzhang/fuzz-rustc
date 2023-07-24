57fd438bcd64427de2da9f267892059bc9aa81d7
Zhang Yuntong <ang.unong@gmail.com>
2023-07-24 11:37:02 +0800

    Fix installation script

--- install.sh
+++ install.sh
@@ -3,7 +3,13 @@
 ## Install dependencies before we can run the actual fuzzer.
 
 # install rust stable
-curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh
+curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf -o install-rustup.sh
+chmod +x install-rustup.sh
+# use default installation mode
+echo "1" | ./install-rustup.sh
+rm install-rustup.sh
+source "$HOME/.cargo/env"
 
 # install rust nightly
 rustup update nightly
+
